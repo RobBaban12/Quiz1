4698bb73413c2fcb542e1127bd7a9ed9f8ec5e32
wends05 <143113418+wends05@users.noreply.github.com>
2024-01-25 00:00:00 +0000

    wee

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,9 @@
 
 
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
-import { motion, scroll } from "framer-motion";
+import { motion, scroll, useAnimation } from "framer-motion";
 
 const navlinks : {
   name: string,
@@ -30,23 +30,29 @@ const navlinks : {
 
 const Header = () => {
 
-
+  const anim = useAnimation()
 
   const [Appear, setAppear] = useState<boolean>(false)
 
-  scroll(progress => console.log(progress))
+  scroll(progress => {
+    console.log(progress)
+    progress >= 0.3 ? anim.start({ y: 0 }) : anim.start({ y: -60 })
+})
 
   return (
-    <nav
+    <motion.nav
       className={`
       fixed flex gap-10 min-h-10 align-middle items-center justify-center bg-zinc-200 dark:bg-zinc-700 w-full
-    `}>
+    `}
+      animate={anim}
+      initial={{ y: -60 }}
+    >
       {
         navlinks.map(({name, href}, index) => 
           <Link key={index} href={href}>{name}</Link>
         )
       }
-    </nav>
+    </motion.nav>
   )
 }
 
